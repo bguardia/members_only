1099067eaf3d1f5ec265ffbc1d1f34be9731363c
Blair Guardia <blairguardia@gmail.com>
2021-05-25 12:56:55 +0900

    Create Post actions and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,73 @@
 class PostsController < ApplicationController
+    before_action :authenticate_member!, only: [:new, :create, :edit, :destroy]
+
+    def index
+      @posts = Post.all.order("created_at DESC")
+    end
+
+    def new
+        @post = current_member.posts.build
+    end
+
+    def show
+       @post = Post.find(params[:id])
+    end
+
+    def create
+        @post = current_member.posts.build(post_params)
+
+        if @post.save
+            flash[:notice] = "Post successfully created!"
+            redirect_to post_path(@post)
+        else
+            flash.now[:alert] = "Post creation failed!"
+            render "new"
+        end
+    end
+
+    def edit
+        begin
+            @post = current_member.posts.find(params[:id])
+        rescue
+            if Post.exists?(params[:id])
+                flash[:alert] = "You do not have permission to edit that post"
+            else
+                flash[:alert] = "Post does not exist"
+            end
+            redirect_to root_url
+        end
+    end
+
+    def update
+       @post = current_member.posts.find(params[:id])
+       
+       if @post.update(post_params)
+          flash[:notice] = "Post successfully updated!"
+          redirect_to post_path(@post)
+       else
+          flash.now[:alert] = "Post update failed!"
+          render "edit"
+       end
+    end
+
+    def destroy
+        begin
+            @post = current_member.posts.find(params[:id])
+        rescue
+            if Post.find_by(id: params[:id])
+                flash[:alert] = "You do not have permission to delete that post"
+            else
+                flash[:alert] = "Post does not exist"
+            end
+        else
+            @post.destroy
+            flash[:notice] = "Post successfuly deleted"
+        end
+      redirect_to root_url
+    end
+
+    private
+    def post_params
+      params.require(:post).permit(:id, :title, :body)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
     has_many :comments
     belongs_to :member
+
+    validates :title, presence: true
+    validates :body, length: { minimum: 10 }
+
 end
